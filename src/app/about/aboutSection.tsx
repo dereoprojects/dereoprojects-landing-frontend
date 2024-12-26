a769faf92f085ff2c1cff99c236fd283eddd94e5
dereoprojects <140266740+dereoprojects@users.noreply.github.com>
2024-12-26 04:13:45 +0300

    save for now

--- src/app/about/aboutSection.tsx
+++ src/app/about/aboutSection.tsx
@@ -55,6 +55,9 @@ const AboutSection = ({
       ref={ref}
       sx={{
         display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        justifyContent: "center",
+        gap: 3,
         alignItems: "center",
         minHeight: "70vh",
         width: "100%", //When i make this 500 px for example, issue is gone
@@ -64,7 +67,14 @@ const AboutSection = ({
       }}
     >
       {/* Left Side: Content */}
-      <Box sx={{ flex: 1, px: 3, height: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          px: 3,
+          height: { xs: "60%", sm: "100%" },
+        }}
+      >
         <motion.div
           style={{
             scale: scaleLeave,
@@ -81,30 +91,53 @@ const AboutSection = ({
               y: yEnter,
             }}
           >
-            <Box display="flex" alignItems="center" gap={2} mb={2}>
-              {icon}
-              <Typography
-                component="div"
-                variant="h3"
-                sx={{ color: reversed ? "secondary.main" : "primary.main" }}
+            <Box
+              display="flex"
+              alignItems="center"
+              sx={{ justifyContent: { xs: "center", sm: "normal" } }}
+              gap={2}
+              mb={2}
+            >
+              <Box
+                display={"flex"}
+                flexDirection={"row"}
+                gap={1}
+                alignItems={"center"}
               >
-                {title}
-              </Typography>
+                {icon}
+                <Typography
+                  component="div"
+                  variant="h3"
+                  sx={{ color: reversed ? "secondary.main" : "primary.main" }}
+                >
+                  {title}
+                </Typography>
+              </Box>
             </Box>
           </motion.div>
         </motion.div>
         <motion.div style={{ opacity: textOpacityLeave, x: xLeave }}>
           <motion.div style={{ opacity: textOpacityEnter, x: xEnter }}>
-            <Typography
-              sx={{ color: reversed ? "text.secondary" : "text.primary" }}
-            >
-              {text}
-            </Typography>
+            <Box sx={{ justifyContent: { xs: "center", sm: "normal" } }}>
+              <Typography
+                sx={{ color: reversed ? "text.secondary" : "text.primary", textAlign: {xs: "center", sm: "start"} }}
+              >
+                {text}
+              </Typography>
+            </Box>
           </motion.div>
         </motion.div>
       </Box>
 
-      <Box sx={{ flex: 1, px: 3, height: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          px: 3,
+          height: { xs: "40%", sm: "100%" },
+          width: { xs: "100%", sm: "50%" },
+        }}
+      >
         <motion.div
           style={{
             display: "flex",
@@ -125,7 +158,11 @@ const AboutSection = ({
             <SvgIcon
               component={animation}
               inheritViewBox
-              sx={{ color: reversed ? "secondary.main" : "primary.main", height:"100%", width:"100%" }}
+              sx={{
+                color: reversed ? "secondary.main" : "primary.main",
+                height: "100%",
+                width: "100%",
+              }}
             />
           </motion.div>
         </motion.div>
